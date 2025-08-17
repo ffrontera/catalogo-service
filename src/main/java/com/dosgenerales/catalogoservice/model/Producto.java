2ff94f58d95499c66d2972cbfbac7275511f1037
ffrontera <ffrontera@live.com>
2025-08-16 23:50:09 -0300

    feat: Add Producto entity

---
+++ src/main/java/com/dosgenerales/catalogoservice/model/Producto.java
@@ -0,0 +1,27 @@
+package com.dosgenerales.catalogoservice.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "productos")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    private String descripcion;
+
+    @Column(nullable = false)
+    private Double precio;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    private String categoria;
+}
