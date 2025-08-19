7ff10bc687b4c648532af400ead1a92aaa4d0315
ffrontera <ffrontera@live.com>
2025-08-18 23:41:22 -0300

    config: Integrate JwtAuthenticationFilter into SecurityFilterChain

--- src/main/java/com/dosgenerales/catalogoservice/config/JwtAuthenticationFilter.java
+++ src/main/java/com/dosgenerales/catalogoservice/config/JwtAuthenticationFilter.java
@@ -12,11 +12,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Collections;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Value("${jwt.decret.key}")
--- src/main/java/com/dosgenerales/catalogoservice/config/SecurityConfig.java
+++ src/main/java/com/dosgenerales/catalogoservice/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.dosgenerales.catalogoservice.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,16 +8,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.GET, "/api/v1/productos/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/productos").hasRole("ADMIN")
