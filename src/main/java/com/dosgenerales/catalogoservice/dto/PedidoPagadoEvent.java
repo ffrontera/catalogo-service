0e563c3c9393f5c2e974f6bf6c488257d4e82077
ffrontera <ffrontera@live.com>
2025-08-21 01:04:06 -0300

    feat: Implement Kafka listener to update product stock

--- src/main/java/com/dosgenerales/catalogoservice/dto/PedidoPagadoEvent.java
+++ src/main/java/com/dosgenerales/catalogoservice/dto/PedidoPagadoEvent.java
@@ -1,4 +1,15 @@
 package com.dosgenerales.catalogoservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PedidoPagadoEvent {
+    private Long  pedidoId;
+    private List<ProductoVendidoDTO> productoVendidos;
 }
--- src/main/java/com/dosgenerales/catalogoservice/dto/ProductoVendidoDTO.java
+++ src/main/java/com/dosgenerales/catalogoservice/dto/ProductoVendidoDTO.java
@@ -1,4 +1,13 @@
 package com.dosgenerales.catalogoservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductoVendidoDTO {
+    private Long productoId;
+    private Integer cantidad;
 }
